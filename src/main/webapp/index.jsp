e0b4cdd746f9d23352d41281b954af800c550902
Andrea Lorenzetti <andrea@ibuildings.it>
2016-10-19 18:24:16 +0200

    Initial release

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java" import="it.ibuildings.mvntutorial.*" %>
+<html>
+<body>
+<h2><% out.println(App.getHelloWorld()); %></h2>
+<h3><% out.println(App.getHelloWorld2()); %></h3>
+</body>
+</html>
---
+++ src/test/java/it/ibuildings/mvntutorial/TestApp1.java
@@ -0,0 +1,14 @@
+package it.ibuildings.mvntutorial;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestApp1 {
+
+	@Test
+	public void testPrintHelloWorld() {
+
+		Assert.assertEquals(App.getHelloWorld(), "Hello World");
+
+	}
+}
